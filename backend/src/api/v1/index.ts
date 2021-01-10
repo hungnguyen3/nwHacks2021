867e321d463e6df6ca4fa835e444ea8374aa5766
nathan <nathan@imashamed.net>
2021-01-09 17:21:45 -0800

    api routes

--- backend/src/api/v1/index.ts
+++ backend/src/api/v1/index.ts
@@ -1,7 +1,5 @@
 import { Router } from 'express';
 
-import v1 from './v1';
-
 const app = Router();
 
 app.get('/', (req, res) => {
@@ -9,7 +7,5 @@ app.get('/', (req, res) => {
         message: req.originalUrl
     })
 });
-
-app.use('/v1', v1);
 
 export default app;
