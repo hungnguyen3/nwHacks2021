e18c551b8fd56813e15c8c162d6d1eadd091a70a
William <williamzhang8@live.ca>
2021-01-10 07:15:24 -0800

    more css login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
 
 import axios, { AxiosResponse } from 'axios';
+import { TextField } from '@material-ui/core';
 interface Props {
     setToken(token: string): void;
 }
@@ -46,24 +48,57 @@ const Login: React.FC<Props> = ({ setToken }) => {
                 <h1>Login</h1>
                 <form onSubmit={handleSubmit}>
                     <label>
-                        <p>Username</p>
-                        <input
+                        <p></p>
+                        <TextField
+                            id="outlined-basic"
+                            variant="outlined"
+                            label="Username"
                             type="text"
                             onChange={e => setUserName(e.target.value)}
                         />
                     </label>
                     <label>
-                        <p>Password</p>
-                        <input
+                        <p></p>
+                        <TextField
+                            id="outlined-basic"
+                            variant="outlined"
+                            label="Password"
                             type="password"
                             onChange={e => setPassword(e.target.value)}
                         />
                     </label>
-                    <div>
-                        <button type="submit">Submit</button>
+                    <div
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <div>
+                            <p> </p>
+                            <Button variant="contained" type="submit">
+                                Submit
+                            </Button>
+                        </div>
                     </div>
                 </form>
-                <button onClick={e => handleRegister(e)}>Register</button>
+                <div
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <div>
+                        <p></p>
+                        <Button
+                            variant="contained"
+                            onClick={e => handleRegister(e)}
+                        >
+                            Register
+                        </Button>
+                    </div>
+                </div>
             </div>
         </div>
     );
