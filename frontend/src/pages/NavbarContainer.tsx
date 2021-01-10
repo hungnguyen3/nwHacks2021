3c2b7fff0124d81e298a9cfd007136df305479f3
Hung Nguyen <hunglnl01@gmail.com>
2021-01-10 11:12:41 -0800

    finish navbar

--- frontend/src/pages/NavbarContainer.tsx
+++ frontend/src/pages/NavbarContainer.tsx
@@ -13,10 +13,10 @@ function NavbarContainer() {
 
     return (
         <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+            <img src={logo} height="80" width="120"></img>
             <Link className="navbar-brand mr-auto" to="/">
                 Home
             </Link>
-            <img src={logo} height="80" width="120"></img>
 
             <button
                 className="navbar-toggler ml-auto"
@@ -36,14 +36,14 @@ function NavbarContainer() {
                 id="navbarNav"
             >
                 <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
+                    {/* <li className="nav-item">
                         <Link className="nav-link" to="/login">
                             Login
                         </Link>
-                    </li>
+                    </li> */}
                     <li className="nav-item">
                         <Link className="nav-link" to="/students">
-                            Student Contacts
+                            Students
                         </Link>
                     </li>
                     <li className="nav-item">
