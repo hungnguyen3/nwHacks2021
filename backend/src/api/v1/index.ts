f8fbf510a057d7496c7b82054bc19a331ecb2346
nathan <nathan@imashamed.net>
2021-01-10 01:41:05 -0800

    eslint

--- backend/src/api/v1/index.ts
+++ backend/src/api/v1/index.ts
@@ -10,13 +10,13 @@ const router = Router();
 
 router.get('/', (req, res) => {
     res.json({
-        message: req.originalUrl
-    })
+        message: req.originalUrl,
+    });
 });
 
 router.use('/contacts', contacts);
 router.use('/login', login);
-router.use('/logout', logout)
+router.use('/logout', logout);
 router.use('/register', register);
 router.use('/homework', homework);
 
--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,26 +1,29 @@
 import { Document, model, Schema } from 'mongoose';
 
 export interface UserDoc extends Document {
-    username: string,
-    password: string,
-    contact: Schema.Types.ObjectId,
-    sessionId: string
+    username: string;
+    password: string;
+    contact: Schema.Types.ObjectId;
+    sessionId: string;
 }
 
 export const userSchema = new Schema({
     username: {
-        type: String, required: true, unique: true
+        type: String,
+        required: true,
+        unique: true,
     },
     password: {
-        type: String, required: true
+        type: String,
+        required: true,
     },
     contact: {
         type: Schema.Types.ObjectId,
-        ref: 'contacts'
+        ref: 'contacts',
     },
     sessionId: {
-        type: String
-    }
+        type: String,
+    },
 });
 
 export const User = model<UserDoc>('users', userSchema);
