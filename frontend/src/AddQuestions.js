797be2479090e36b7484aff18fe273a2ad58743a
William <williamzhang8@live.ca>
2021-01-09 17:10:49 -0800

    Added Login + ManageStudent + AddQuestion + SendSMS pages

---
+++ frontend/src/AddQuestions.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddQuestions() {
+    return (
+        <div>
+            Add
+        </div>
+    )
+}
+
+export default AddQuestions
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './Login.js';
+import Managestudents from './ManageStudents';
+import AddQuestions from './AddQuestions';
+import SendSMS from './SendSMS';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Login></Login>
+      <Managestudents></Managestudents>
+      <AddQuestions></AddQuestions>
+      <SendSMS></SendSMS>
     </div>
   );
 }
